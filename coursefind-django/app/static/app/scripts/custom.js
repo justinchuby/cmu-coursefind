f37f5d83df8a4c0c36f0ff8160dfd365a9bf510a
justinchuby <justinchuby@foxmail.com>
2016-07-23 21:46:57 -0400

    Back button; 404 page

--- coursefind-django/app/static/app/scripts/custom.js
+++ coursefind-django/app/static/app/scripts/custom.js
@@ -21,7 +21,7 @@ function clearForm()
     $('#search-text').focus();
 }
 
-function getDetailPgaeColor(courseid) {
+function getDetailPageColor(courseid) {
     var dept = courseid.slice(0,2);
     var level = parseInt(courseid.slice(3,6));
     var color = {
@@ -136,8 +136,8 @@ function getDetailPgaeColor(courseid) {
     return pageColors
 }
 
-function setDetailPgaeColor(courseid) {
-    pageColors = getDetailPgaeColor(courseid);
+function setDetailPageColor(courseid) {
+    pageColors = getDetailPageColor(courseid);
     $('.jc-major-color').addClass(pageColors.major_color);
     $('.jc-text-major').addClass(pageColors.text_major);
     $('.jc-text-courseid').addClass(pageColors.text_courseid);
