f460a28ba9c67dd5dd9c3659de4f70d303976d38
justinchuby <justinchuby@foxmail.com>
2016-07-24 09:15:15 -0400

    Change a way to stay standalone

--- coursefind-django/app/static/app/scripts/custom.js
+++ coursefind-django/app/static/app/scripts/custom.js
@@ -1,17 +1,17 @@
-//$('.datepicker').pickadate({
-//    selectMonths: true, // Creates a dropdown to control month
-//    selectYears: true, // Creates a dropdown of 15 years to control year
-//    format: 'd mmm, yyyy',
-//  });
-//
-//$('.timepicker').pickatime({
-//      twelvehour: false
-//    });
-
 // Initialize collapse button
 $(".button-collapse").sideNav();
 // Initialize collapsible (uncomment the line below if you use the dropdown variation)
 //$('.collapsible').collapsible();
+
+var a=document.getElementsByTagName("a");
+for(var i=0;i<a.length;i++) {
+    if(!a[i].onclick && a[i].getAttribute("target") != "_blank") {
+        a[i].onclick=function() {
+                window.location=this.getAttribute("href");
+                return false; 
+        }
+    }
+}
 
 // clearForm() from http://stackoverflow.com/questions/6653556/jquery-javascript-function-to-clear-all-the-fields-of-a-form answer by ktamlyn
 function clearForm()
