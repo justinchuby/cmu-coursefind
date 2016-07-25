5bbe4080bdf307cb70f9374e14ed6a199f056abd
justinchuby <justinchuby@foxmail.com>
2016-07-25 11:18:14 -0400

    Change the sitemap url

--- coursefind-django/coursefind/urls.py
+++ coursefind-django/coursefind/urls.py
@@ -27,8 +27,8 @@ urlpatterns = [
     url(r'^courses/(?P<courseid>\d{2}-\d{3})[/]?$', app.views.course_detail),
     url(r'^(?P<index>(f|s|m1|m2)\d{2})/(?P<courseid>\d{5})[/]?$', app.views.redirect_to_course_detail),
     url(r'^courses/(?P<courseid>\d{5})[/]?$', app.views.redirect_to_course_detail),
-    url(r'^(?P<index>(f|s|m1|m2)\d{2})/sitemap\.txt$', app.views.sitemap),
-    url(r'^courses/sitemap\.txt$', app.views.sitemap),
+    url(r'^sitemaps/sitemap-(?P<index>(f|s|m1|m2)\d{2})\.txt$', app.views.sitemap),
+    url(r'^sitemaps/sitemap-current-courses\.txt$', app.views.sitemap),
     url(r'^[/]?$', app.views.home),
     url(r'^', app.views.redirect_to_home)
 ]
