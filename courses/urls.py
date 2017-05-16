0b0cae98700b056393cb07a8ba36a27d42481f34
maxcccccc <zizhuomaxchen@gmail.com>
2017-05-15 22:15:35 -0400

    Add error handling in courses view

--- courses/urls.py
+++ courses/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'courses'
 
 urlpatterns = [
-    url(r'^(?P<courseid>\d{2}-\d{3})/(?P<index>(f|s|m1|m2)\d{2})[/]?$', views.CourseDetailView.as_view()),
+    url(r'^(?P<courseid>\d{2}-\d{3})/(?P<course_index>(f|s|m1|m2)\d{2})[/]?$', views.CourseDetailView.as_view()),
     url(r'^(?P<courseid>\d{2}-\d{3})[/]?$', views.CourseDetailView.as_view())
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, HttpResponseServerError
 from django.views import View
 import requests
 import json
@@ -18,17 +18,20 @@ class CourseDetailView(View):
         response = requests.get(req)
         # TODO error handling
         response_dict = response.json()
-        if 'error' not in response_dict:
-            course = Course(response_dict['course'])
-            context = {
-                'page': 'course_detail',
-                'search_index': None,
-                'course_index': course_index,
-                'catalog_semester': course.semester_current,
-                'catalog_date': course.rundate,
-                'course': course
-            }
-            return render(request, 'courses/course_detail.html', context)
+        try:
+            if 'error' not in response_dict:
+                course = Course(response_dict['course'])
+                context = {
+                    'page': 'course_detail',
+                    'search_index': None,
+                    'course_index': course_index,
+                    'catalog_semester': course.semester_current,
+                    'catalog_date': course.rundate,
+                    'course': course
+                }
+                return render(request, 'courses/course_detail.html', context)
+        except TypeError:
+            raise HttpResponseServerError()
 
         # TODO Add different error pages
         raise Http404("No info about {} in {}".format(courseid, course_index))
