0d7b36461a975d184e8215fb7d50a367be505090
justinchuby <justinchuby@foxmail.com>
2017-07-01 23:37:56 -0400

    Update link in navbar and format

--- frontend/src/js/components/Navbar.js
+++ frontend/src/js/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 import { randomPick } from '../helpers';
 import '../../css/Navbar.css';
 
@@ -28,16 +29,30 @@ class Navbar extends Component {
       <div className="navbar-fixed" style={{height: '100px'}}>
         <nav>
           <div className="nav-wrapper">
-            <a data-activates="slide-out" className="button-collapse hide-on-med-and-up"><i className="material-icons">menu</i></a>
-            <a href="/" className="brand-logo left hide-on-small-only light" id="brand-logo">&nbsp; Course Find</a>
+            <a data-activates="slide-out"
+              className="button-collapse hide-on-med-and-up">
+                <i className="material-icons">menu</i>
+            </a>
+            <Link to="/"
+              className="brand-logo left hide-on-small-only light"
+              id="brand-logo">
+              &nbsp; Course Find
+            </Link>
             <div className="row" style={{ height: '100%' }}>
-              <form method="GET" action="/" className="col s8 m10 l11" id="search-box">
+              <form method="GET"
+                action="/"
+                className="col s8 m10 l11"
+                id="search-box">
                 <div className="input-field">
                   <input type="search" name="q" className="field"
-                    required maxLength="100" placeholder={this.state.searchPrompt} id="search-text" 
+                    required maxLength="100"
+                    placeholder={this.state.searchPrompt}
+                    id="search-text" 
                     onFocus={this.handleFocus.bind(this)}
                     onBlur={this.handleBlur.bind(this)}></input>
-                  <label className="label-icon" htmlFor="search-text"><i className="material-icons">search</i></label>
+                  <label className="label-icon" htmlFor="search-text">
+                    <i className="material-icons">search</i>
+                  </label>
                   <i className="material-icons">close</i>
                 </div>
               </form>
