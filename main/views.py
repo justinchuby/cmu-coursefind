5d9b922f0c4b04177d9d36b6fe9d765c3641093c
justinchuby <justinchuby@foxmail.com>
2017-07-30 13:05:26 -0400

    Django clean up

--- main/views.py
+++ main/views.py
@@ -1,31 +1,7 @@
 from django.shortcuts import render
 from django.views import View
-# import datetime
-# import shared.utils
 
 
 class Home(View):
     def get(self, request):
         return render(request, 'main/base.html')
-
-
-# class About(View):
-#     def get(self, request):
-#         current_semester = \
-#             shared.utils.get_semester_from_date(datetime.date.today())
-#         context = {
-#             'page': 'about',
-#             'current_semester': current_semester
-#         }
-#         return render(request, 'main/about.html', context)
-
-
-# class Disclaimer(View):
-#     def get(self, request):
-#         current_semester = \
-#             shared.utils.get_semester_from_date(datetime.date.today())
-#         context = {
-#             'page': 'disclaimer',
-#             'current_semester': current_semester
-#         }
-#         return render(request, 'main/disclaimer.html', context)
