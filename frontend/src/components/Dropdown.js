cfcb7c15b480c32b874f314f864805b587240bb0
justinchuby <justinchuby@foxmail.com>
2017-07-30 23:33:36 -0400

    Update dropdown style

--- frontend/src/components/Dropdown.js
+++ frontend/src/components/Dropdown.js
@@ -9,7 +9,7 @@ class Dropdown extends Component {
     $('.dropdown-button').dropdown({
         inDuration: 300,
         outDuration: 225,
-        constrainWidth: true, // Does not change width of dropdown to that of the activator
+        constrainWidth: false, // Does not change width of dropdown to that of the activator
         hover: false, // Activate on hover
         gutter: 0, // Spacing from edge
         belowOrigin: false, // Displays dropdown below the button
