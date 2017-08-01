6db28d7c4d5f67ed5f03b4742fd2da855dd95792
justinchuby <justinchuby@foxmail.com>
2017-08-01 10:42:33 -0400

    Update Django url paths

--- main/views.py
+++ main/views.py
@@ -4,4 +4,8 @@ from django.views import View
 
 class Home(View):
     def get(self, request):
-        return render(request, 'main/base.html')
+        return render(request, 'main/index.html')
+
+
+def page_not_found(request):
+    return render(request, 'main/index.html')
